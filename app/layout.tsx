b18a44c8e36501cfb5e6cdee8c36e208683877f8
xyzprtk <prtk.2403@gmail.com>
2026-03-27 15:55:58 +0530

    feat: set dark mode as default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange={false}
         >
           {children}
